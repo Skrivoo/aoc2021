beb6540bcebe0f867005b1b4fa14361020205ba8
Skrivoo <pedrusz96@gmail.com>
2024-10-27 19:14:07 +0100

    complete day2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,10 @@
 import Day1.dayOneFirst
 import Day1.dayOneSecond
+import Day2.dayTwoFirst
+import Day2.dayTwoSecond
 
 @main def hello(): Unit =
-  println(dayOneFirst)
-  println(dayOneSecond)
+  println(s"DAY1-1 $dayOneFirst")
+  println(s"DAY1-2 $dayOneSecond")
+  println(s"DAY2-1 $dayTwoFirst")
+  println(s"DAY2-2 $dayTwoSecond")
