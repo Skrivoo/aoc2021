30d9ebc03cdc4ef43185484d08970af87a7f4bea
Skrivoo <pedrusz96@gmail.com>
2024-10-28 20:16:30 +0100

    complete day3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,15 @@ import Day1.dayOneFirst
 import Day1.dayOneSecond
 import Day2.dayTwoFirst
 import Day2.dayTwoSecond
+import Day3.dayThreeFirst
+import Day3.negateBinaryNumber
+import Day3.getTheDecimalValueOfBinary
+import Day3.dayThreeSecond
 
 @main def hello(): Unit =
   println(s"DAY1-1 $dayOneFirst")
   println(s"DAY1-2 $dayOneSecond")
   println(s"DAY2-1 $dayTwoFirst")
   println(s"DAY2-2 $dayTwoSecond")
+  println(s"DAY3-1 $dayThreeFirst")
+  println(s"DAY3-2 $dayThreeSecond")
